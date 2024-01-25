63aa91a71b639a03e806c0a4b1639e41851ce7de
Alberto-Arrighetti <alberto.arrighetti1571@gmail.com>
2024-01-25 16:19:38 +0100

    variabili

---
+++ js/script.js
@@ -0,0 +1,32 @@
+let firstName;
+firstName = prompt("Inserisci il tuo nome");
+
+let lastName;
+lastName = prompt("Inserisci il tuo cognome");
+
+let color;
+color = prompt("Inserisci il tuo colore preferito");
+
+
+const number = 23
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+console.log(firstName);
+console.log(lastName);
+console.log(color);
